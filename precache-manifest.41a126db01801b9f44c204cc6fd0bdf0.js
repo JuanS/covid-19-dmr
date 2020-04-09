05472619913da6bbf0c92adc2e69e9324084f378
Juan Sierra <contact.juan.sierra@gmail.com>
2020-04-10 01:16:39 +0200

    Updates

---
+++ precache-manifest.41a126db01801b9f44c204cc6fd0bdf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382a076b792847597d77223133f10d0c",
+    "url": "/covid-19-dmr/index.html"
+  },
+  {
+    "revision": "8ab5825d4b31f9012023",
+    "url": "/covid-19-dmr/static/css/main.32fb660c.chunk.css"
+  },
+  {
+    "revision": "856051e8060b39007242",
+    "url": "/covid-19-dmr/static/js/2.de971caa.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/covid-19-dmr/static/js/2.de971caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab5825d4b31f9012023",
+    "url": "/covid-19-dmr/static/js/main.3792567b.chunk.js"
+  },
+  {
+    "revision": "68394cc77469d10b27a1",
+    "url": "/covid-19-dmr/static/js/runtime-main.1d6be6ad.js"
+  }
+]);
